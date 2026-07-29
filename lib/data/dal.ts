232743be10cda55df91cb3b4099d8c87203e6bdf
Quiatus <Quiatus@gmail.com>
2026-07-29 12:09:55 +0200

    add resource updater

--- lib/data/dal.ts
+++ lib/data/dal.ts
@@ -14,3 +14,19 @@ export const getData = cache(async function <T>(tableName: string): Promise<T> {
 
   return data as T;
 })
+
+export async function updateData<T>(tableName: string, data: Partial<T>): Promise<T> {
+  const { data: updatedRow, error } = await supabase
+    .from(tableName)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    .update(data as any)
+    .eq("id", 1)
+    .select() 
+    .single();
+
+  if (error) {
+    throw new Error(`Failed to update ${tableName}`); 
+  }
+
+  return updatedRow as T;
+}
